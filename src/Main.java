b8735752ae5fbd1f11ccc57045bb67d287f08c95
Grey Cat <greycatmadcat@gmail.com>
2022-09-30 16:48:51 +0700

    Read expression from file

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,26 @@
 import com.calculator.Calculator;
 import com.calculator.CalculatorException;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        String filename = "input.txt";
+        Scanner scanner;
+        try {
+            scanner = new Scanner(new File(filename));
+        } catch (FileNotFoundException e) {
+            System.out.println("Error! " + filename + " not found");
+            return;
+        }
 
         Calculator calculator = new Calculator();
+        String line = scanner.nextLine();
+        System.out.println(line);
         try {
-            System.out.println(calculator.calculateString(scanner.nextLine()));
+            System.out.println(calculator.calculateString(line));
         } catch (CalculatorException exception) {
             System.out.println(exception.getMessage());
         }
